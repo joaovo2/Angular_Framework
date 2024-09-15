e27e47557ecd0ed122dfc60c301e2a297155359c
Joao Vitor <joaovitoroliveirasoarespereira@gmail.com>
2024-09-14 23:34:55 -0300

    -*-*-*-*-*-*-*-*-*-*

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomepageComponent } from "./components/homepage/homepage.component";
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+@Component
+({
+    selector: 'app-root',
+    standalone: true,
+    imports: [
+    RouterOutlet,
+    HomepageComponent
+],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'LearningNgUsingDocumentation';
+export class AppComponent
+{
+
 }
---
+++ src/app/components/homepage/homepage.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+    selector: 'app-homepage',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './homepage.component.html',
+    styleUrl: './homepage.component.css',
+})
+export class HomepageComponent
+{
+    
+}
